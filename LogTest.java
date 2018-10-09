ac7fbb16e8b97ef200c358e71300c09f5f26ac4d
Nihara Nelligahawatta <30996287+niharai@users.noreply.github.com>
2018-10-10 04:22:41 +0530

    Log Test cases

---
+++ LogTest.java
@@ -0,0 +1,51 @@
+package com.procurement_system_backend.backend;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LogTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	
+	/**
+	 * @throws java.lang.Exception
+	 * @return void
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 * @return void
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 * @return void
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 * @return void
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
