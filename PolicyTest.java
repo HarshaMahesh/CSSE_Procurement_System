997a60b3c31aa33638309ed819c3d68169983deb
Nihara Nelligahawatta <30996287+niharai@users.noreply.github.com>
2018-10-10 04:19:54 +0530

    Policy Test Cases

---
+++ PolicyTest.java
@@ -0,0 +1,14 @@
+package com.procurement_system_backend.backend;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PolicyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
